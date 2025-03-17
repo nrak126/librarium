ba730c96a70475925a0462609ae3094142333590
sana-sagegami <sana0123737@gmail.com>
2025-03-17 21:43:09 +0900

    新しいBookInfoコンポーネントとBtnコンポーネントを追加

---
+++ src/components/book/Btn/index.tsx
@@ -0,0 +1,9 @@
+'use client';
+export const Btn = () => {
+    const onClickButton = () => alert(" ");
+    return (
+        <div>
+            <button onClick={onClickButton}>戻る</button>
+        </div>
+    );
+}
