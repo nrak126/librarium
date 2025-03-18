ca9732699fa058bb1a1bbf8c09a8dda0017a1940
sana-sagegami <sana0123737@gmail.com>
2025-03-18 21:20:01 +0900

    ボタンコンポーネントから位置指定プロパティを削除

--- src/components/book/Btn/index.tsx
+++ src/components/book/Btn/index.tsx
@@ -7,33 +7,18 @@ interface BtnProps {
   text: string;
   bgColor: string;
   handleClick: () => void;
-  position?: "relative" | "absolute" | "fixed" | "sticky" | "static";
-  top?: string;
-  left?: string;
-  right?: string;
-  bottom?: string;
 }
 
 export const Btn: React.FC<BtnProps> = ({
   text,
   bgColor,
   handleClick,
-  position,
-  top,
-  left,
-  right,
-  bottom,
 }) => {
   return (
     <button
       className={classes.BaseBtn}
       style={{
         backgroundColor: bgColor,
-        position: position || "relative",
-        top: top || "auto",
-        left: left || "auto",
-        right: right || "auto",
-        bottom: bottom || "auto",
       }}
       onClick={handleClick}
     >
