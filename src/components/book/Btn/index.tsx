693f4ffd53c12c60a2e1d8c1d7074370790c4f9f
sana-sagegami <sana0123737@gmail.com>
2025-03-18 18:58:08 +0900

    Btnコンポーネントのスタイルを追加し、プロパティを受け取るように変更

--- src/components/book/Btn/index.tsx
+++ src/components/book/Btn/index.tsx
@@ -1,9 +1,23 @@
-'use client';
-export const Btn = () => {
-    const onClickButton = () => alert(" ");
-    return (
-        <div>
-            <button onClick={onClickButton}>戻る</button>
-        </div>
-    );
+"use client";
+import React from "react";
+import classes from "./index.module.scss";
+
+interface BtnProps {
+    text: string;
+    bgColor: string;
+    handleClick?: () => void;
 }
+
+export const Btn: React.FC<BtnProps> = ({ text, bgColor, handleClick }) => {
+    return (
+    
+        <button
+            className={classes.BaseBtn}
+            style={{ backgroundColor: bgColor }}
+            onClick={handleClick}
+            >
+            {text}
+        </button>
+    
+    );
+};
