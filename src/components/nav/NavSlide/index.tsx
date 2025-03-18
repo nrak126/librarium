46c3214208ce0fa253d62ed244aca5121777a207
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-03-18 21:30:00 +0900

    見た目完成

--- src/components/nav/NavSlide/index.tsx
+++ src/components/nav/NavSlide/index.tsx
@@ -2,17 +2,26 @@
 
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+
 import { NavIcon } from "../NavIcon";
-import "swiper/css"; // Swiper のスタイルをインポート
+import "swiper/css";
+import "swiper/scss/pagination";
+import { Pagination } from "swiper/modules";
+import "./index.scss"; // 追加したスタイルをインポート
 
 export const NavSlide: React.FC = () => {
   return (
     <div style={{ position: "relative", width: "100%" }}>
       <Swiper
-        spaceBetween={70} // アイコン間の隙間
-        slidesPerView={3} // 一画面に3つのアイコンを表示
+        className="swiper-container"
+        spaceBetween={15} // アイコン間の隙間
+        slidesPerView={2.5} // 一画面に2.5つのアイコンを表示
         loop={true} // ループ
         navigation={true} // ナビゲーションボタン
+        modules={[Pagination]}
+        pagination={{
+          clickable: true,
+        }}
         centeredSlides={true}
       >
         <SwiperSlide>
