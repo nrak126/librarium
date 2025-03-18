8c1fc37af33a8fecc4b5f02f353b590e510cf615
kkkk0113 <koto1658@gmail.com>
2025-03-18 13:18:11 +0900

    authページのスタイルをSCSSで追加し、サインイン・サインアップボタンの機能を実装

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,71 +1,42 @@
 "use client";
 
 import { useState } from "react";
-import  Image  from "next/image";
+import Image from "next/image";
 import Icon from "@/public/icon.svg";
+import styles from "./auth.module.scss";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
   const [signIn, setSignIn] = useState(false);
+  const [signUp, setSignUp] = useState(false);
+  const router = useRouter();
 
-  const SignIn = () => {
+  const handleSignIn = () => {
     setSignIn(true);
-    console.log("useState", signIn);
+    console.log("SignInボタンが押されました");
+    router.push("/");
   };
+
+  const handleSignUp = () => {
+    setSignUp(true);
+    console.log("SignUpボタンが押されました");
+  };
+
   return (
-    <div
-      style={{
-        background: "#FFFBF5",
-        display: "flex",
-        height: "100vh",
-        width: "100hv",
-        // justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
-    >
-      {/* <img
-        src="/icon.svg"
-        alt="icon"
-        style={{
-          marginTop: "15%",
-          marginBottom: "50%",
-          width: "55%",
-          height: "25%",
-        }}
-      /> */}
-      <Image src={Icon} alt={'librariumのアイコン'} width={210} height={210} />
-      <button
-        onClick={SignIn}
-        style={{
-          border: "2px solid #635038",
-          borderRadius: 30,
-          color: "#635038",
-          height: "38px",
-          width: "65%",
-          marginBottom: "7%",
-          background: "#FFFBF5",
-          boxShadow: "1px 1px 4px gray",
-          fontWeight: "bold",
-          fontSize: "20px",
-        }}
-      >
+    <div className={styles.whole}>
+      <Image
+        src={Icon}
+        alt={"librariumのアイコン"}
+        width={230}
+        height={230}
+        className={styles.icon}
+      />
+
+      <button onClick={handleSignIn} className={styles.signInBtn}>
         サインイン
       </button>
-      <button
-        onClick={SignIn}
-        style={{
-          border: "2px solid #635038",
-          borderRadius: 30,
-          color: "#635038",
-          left: "20%",
-          width: "65%",
-          height: "38px",
-          background: "#FFFBF5",
-          boxShadow: "1px 1px 4px gray",
-          fontWeight: "bold",
-          fontSize: "20px",
-        }}
-      >
+
+      <button onClick={handleSignUp} className={styles.signUpBtn}>
         サインアップ
       </button>
     </div>
