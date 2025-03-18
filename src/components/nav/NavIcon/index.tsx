da0058720c0b44f2b73ae73e94e89ae334dc075a
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-03-18 18:49:41 +0900

    完成

--- src/components/nav/NavIcon/index.tsx
+++ src/components/nav/NavIcon/index.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import React from "react";
 import style from "./index.module.scss";
-import iconSvg from "@/public/icon.svg"; // ここで直接import
+import iconSvg from "@/public/icon.svg";
 
 export const NavIcon: React.FC = () => {
   return (
     <div className={style.content}>
       <Image src={iconSvg} alt="icon" width={89} height={89} />
+      <p className={style.name}>貸出</p>
     </div>
   );
 };
