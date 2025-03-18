83f2ef27892eda530ddc00d5e79ddf50329d0b30
nrak126 <daichidaichi126@gmail.com>
2025-03-18 10:23:50 +0900

    BooksListコンポーネントを追加し、ページコンポーネントをリファクタリング

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,33 +1,12 @@
-"use client";
-import { useEffect, useState } from "react";
-import type { Book } from "@/src/types/book";
-import Link from "next/link";
+import BooksList from "./components/BooksList";
 
-export default function Page() {
-  const [books, setBooks] = useState<Book[]>([]);
-
-  useEffect(() => {
-    (async () => {
-      const response = await fetch("/api/books");
-      const data = await response.json();
-      setBooks(data);
-    })();
-  }, []);
+export default async function Page() {
+  // ここでサーバーから初期データ取得してもいい（任意）
+  // const books = await fetchBooks();
 
   return (
-    <>
-      <h1>本のリスト</h1>
-      <ul>
-        {books.map((book) => (
-          <li key={book.id}>
-            <h3>{book.title} </h3>
-            <p>{book.author}</p>
-            <Link href={`book/${book.id}`}>
-            <button>詳細GO</button>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </>
+    <div>
+      <BooksList />
+    </div>
   );
 }
