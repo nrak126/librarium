41524f8b8c96c4bba0612f7ff4337c609acd3282
kkkk0113 <koto1658@gmail.com>
2025-03-19 17:53:43 +0900

    新しいユーザー一覧ページとスタイルを追加し、グローバルフォントを設定

---
+++ src/app/users/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import styles from "./users.module.scss";
+import { SearchBar } from "../../components/SearchBar/index";
+
+export default function Page() {
+  return (
+    <>
+      <div className={styles.whole}>
+        <div className={styles.title}>利用者一覧</div>
+
+        <div className={styles.bar}>
+          <SearchBar func={"ユーザー検索"} />
+        </div>
+      </div>
+
+      <div className={styles.myprofile}>MY PROFILE</div>
+
+      {/* <div className={styles.userProfile}>
+        <div className={styles.icon}>アイコン</div>
+        <div className={styles.numname}>学生番号名前</div>
+        <div className={styles.level}>Lv.1</div>
+        <div className={styles.tag}>#フロントビギナー</div>
+      </div> */}
+    </>
+  );
+}
