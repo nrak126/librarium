c186afa9528d33491195afcf7c54fabd9150243b
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-03-20 19:47:11 +0900

    できた

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,22 @@
+"use client";
+
 import React from "react";
 import styles from "./index.module.scss";
 import { HamBtn } from "@/src/components/HamBtn";
+import { useRouter } from "next/navigation";
 
 export const Header = () => {
+  const router = useRouter();
+  const link = "/";
+
+  const onHome = (link: string) => {
+    router.push(link); // クリック時に指定されたリンクに遷移
+  };
   return (
     <div className={styles.header}>
-      <h1 className={styles.h1}>Librarium</h1>
+      <h1 onClick={() => onHome(link)} className={styles.h1}>
+        Librarium
+      </h1>
       <HamBtn />
       {/* <div className={styles.section} /> */}
     </div>
