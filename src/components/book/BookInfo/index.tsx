1c026a481464a39d18a50f42cb6444d5ee82dc1c
sana-sagegami <sana0123737@gmail.com>
2025-03-24 18:11:26 +0900

    本の情報コンポーネントのスタイルを調整し、画像表示を本のカードコンポーネントに置き換え

--- src/components/book/BookInfo/index.tsx
+++ src/components/book/BookInfo/index.tsx
@@ -4,21 +4,14 @@ import React from "react";
 import classes from "./index.module.scss";
 import Image from "next/image";
 import { Book } from "@/src/types/book";
+import { BookCard } from "../BookCard";
 
 export const BookInfo = ({ book }: { book: Book }) => {
   return (
     <div className={classes.BookInfo}>
       <p className={classes.Title}>{book.title}</p>
       <hr className={classes.hr} />
-      <div className={classes.BookImg}>
-        <Image
-          className={classes.Img}
-          src={book.thumbnail}
-          alt="本の表紙画像"
-          width={100}
-          height={100}
-        />
-      </div>
+      <BookCard book={book} />
       <div className={classes.InfoBox}>
         <div className={classes.AuthorLabel}>
           <p className={classes.Label}>著者</p>
