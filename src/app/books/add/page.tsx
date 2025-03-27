c757fb713fd755914c730fd552619e5ce7694364
nrak126 <daichidaichi126@gmail.com>
2025-03-27 20:28:26 +0900

    Supabaseを使用して本の登録機能を実装し、Firebaseの依存を削除

--- src/app/books/add/page.tsx
+++ src/app/books/add/page.tsx
@@ -31,8 +31,8 @@ const Page = () => {
             publisher: volumeInfo.publisherName || "出版会社情報がありません",
             stock: 1, // 数量のデフォルト値
             available: 1, // 数量のデフォルト値
-            tags: volumeInfo.tags, // タグのデフォルト値
-            created_at: volumeInfo.createdAt, // 作成日時
+            tags: [], // タグのデフォルト値
+            created_at: new Date().toISOString(), // 作成日時のデフォルト値
           };
           setBookInfo(fetchedBook); // 本の情報をセット
         } else {
