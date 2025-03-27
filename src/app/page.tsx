db8c9825ce81abf106e261f98fd5d69b0ab5b932
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-03-27 16:24:01 +0900

    貸し出し中の見た目と、Tabができた

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@
 import { useState } from "react";
 import { SearchBar } from "../components/SearchBar";
 import { NavSlide } from "../components/nav/NavSlide";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import styles from "./styles/TabBar.module.scss";
+import { RentalTime } from "../components/book/RentalTime";
 
 export default function Home() {
   const [searchCilck, setSearchCilck] = useState(false);
@@ -17,6 +20,30 @@ export default function Home() {
         setSearchName={setSearchName}
       />
       <NavSlide />
+
+      <Tabs className={styles.tabs}>
+        <TabList className={styles.tabList}>
+          <Tab className={styles.tab} selectedClassName={styles.tabSelected}>
+            ランキング
+          </Tab>
+          <Tab className={styles.tab} selectedClassName={styles.tabSelected}>
+            貸出中
+          </Tab>
+          <Tab className={styles.tab} selectedClassName={styles.tabSelected}>
+            おすすめ
+          </Tab>
+        </TabList>
+
+        <TabPanel>
+          <h2>Any content 1</h2>
+        </TabPanel>
+        <TabPanel>
+          <RentalTime />
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+      </Tabs>
     </>
   );
 }
