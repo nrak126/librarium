c8e59f63776c215f4f1fc677facd48644a626b9b
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-03-28 23:52:59 +0900

    見た目だけ完成

---
+++ src/app/books/return/components/MyData.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Image from "next/image";
+import style from "../style/return.module.scss";
+import Icon from "../icon/fu_icon.jpg";
+import BookIcon from "../icon/img.jpg";
+
+export const MyData = () => {
+  return (
+    <div>
+      <div className={style.contents}>
+        <div className={style.content}>
+          <Image
+            src={Icon}
+            alt={"librariumのアイコン"}
+            width={57}
+            height={57}
+            className={style.icon}
+          />
+          <div className={style.text}>
+            <p className={style.bookName}>Iphone開発アプリ</p>
+            <p className={style.return}>
+              返却期限：<span className={style.returnTime}>2025/99/99</span>
+            </p>
+            <p className={style.userName}>あと3日</p>
+          </div>
+          <Image
+            src={BookIcon}
+            alt={"librariumのアイコン"}
+            width={60}
+            height={90}
+            className={style.BookIcon}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/app/books/return/components/Return.tsx
+++ src/app/books/return/components/Return.tsx
@@ -1,8 +1,6 @@
 import { SearchBar } from "@/src/components/SearchBar";
 import style from "../style/return.module.scss";
-import Image from "next/image";
-import Icon from "./fu_icon.jpg";
-import BookIcon from "./img.jpg";
+import { MyData } from "./MyData";
 
 export const Return: React.FC = () => {
   return (
@@ -12,31 +10,8 @@ export const Return: React.FC = () => {
         <SearchBar func={"検索"} />
       </div>
       <p className={style.myProfile}>MY RENTAL BOOK</p>
-      <div className={style.contents}>
-        <div className={style.content}>
-          <Image
-            src={Icon}
-            alt={"librariumのアイコン"}
-            width={57}
-            height={57}
-            className={style.icon}
-          />
-          <div className={style.text}>
-            <p className={style.bookName}>Iphone開発アプリ</p>
-            <p className={style.return}>
-              返却期限：<span className={style.returnTime}>2025/99/99</span>
-            </p>
-            <p className={style.userName}>あと3日</p>
-          </div>
-          <Image
-            src={BookIcon}
-            alt={"librariumのアイコン"}
-            width={60}
-            height={90}
-            className={style.BookIcon}
-          />
-        </div>
-      </div>
+      <MyData />
+      <p className={style.allProfile}>ALL RENTAL BOOK</p>
     </>
   );
 };
