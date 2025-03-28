2a2259bd15a03678473cdeb8804c06a557def7ec
kkkk0113 <koto1658@gmail.com>
2025-03-29 00:02:15 +0900

    ユーザー詳細コンポーネントを追加し、タグ編集機能を実装。スタイルを調整。

---
+++ src/app/users/[id]/components/UserDtile.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import styles from "./UsersDtile.module.scss";
+import Image from "next/image";
+import Icon from "@/public/icon.svg";
+import { TagList } from "@/src/components/Users/TagList";
+import { TagEdit } from "@/src/components/Users/TagEdit";
+import { useState } from "react";
+
+export default function UserDtile() {
+  const [clickEditer, setClickEditer] = useState(false);
+
+  const handleSample = () => {
+    setClickEditer(!clickEditer);
+    console.log(`ClickEdit ${clickEditer}`);
+    console.log("編集が押されました。");
+  };
+
+  return (
+    <div className={styles.whole}>
+      <Image
+        src={Icon}
+        alt={"ユーザーのアイコン"}
+        width={180}
+        height={180}
+        className={styles.icon}
+        priority
+      />
+
+      <div className={styles.username}>k24142 矢部 大智</div>
+
+      <div className={styles.tagediter}>
+        <div className={styles.tag}>タグ</div>
+        {clickEditer ? (
+          <button onClick={handleSample} className={styles.editbutton}>
+            編集
+          </button>
+        ) : (
+          <button onClick={handleSample} className={styles.editbutton}>
+            完了
+          </button>
+        )}
+      </div>
+
+      {clickEditer ? <TagList /> : <TagEdit />}
+
+      <div className={clickEditer ? styles.trueexp : styles.falseexp}>
+        現在の経験値
+      </div>
+    </div>
+  );
+}
