2845563f64d76dc012b5169f470a495cc52ae106
sana-sagegami <sana0123737@gmail.com>
2025-03-29 14:10:48 +0900

    StockStateコンポーネントを初期書籍情報を受け取るように変更し、在庫管理機能を追加

--- src/app/books/components/BooksList.tsx
+++ src/app/books/components/BooksList.tsx
@@ -34,7 +34,7 @@ export default function BooksList() {
               <BookCardList book={book} />
             </Link>
             <div className={styles.stock}>
-              <StockState isBorrowed={isBorrowed} />
+              <StockState initialBook={book} />
             </div>
           </div>
         </li>
--- src/components/book/StockState/index.tsx
+++ src/components/book/StockState/index.tsx
@@ -2,24 +2,50 @@
 
 import React from "react";
 import classes from "./index.module.scss";
+import { Book } from "@/src/types";
+import { useState } from "react";
 
 interface StockStateProps {
-  isBorrowed: boolean;
+  initialBook: Book;
 }
 
-export const StockState: React.FC<StockStateProps> = ({ isBorrowed }) => {
+export const StockState: React.FC<StockStateProps> = ({ initialBook }) => {
+  const [book, setBook] = useState(initialBook);
+
+  const handleIncreaseAvailable = () => {
+    setBook((prev) => ({
+      ...prev,
+      available: prev.available + 1,
+      stock: prev.stock - 1,
+    }));
+  };
+
+  const handleDecreaseAvailable = () => {
+    if (book.available > 0) {
+      setBook((prev) => ({
+        ...prev,
+        available: prev.available - 1,
+        stock: prev.stock + 1,
+      }));
+    }
+  };
+
+  const isBorrowed = book.available === 0;
+
   return (
-    <div
-      className={`${classes.StockState} ${
-        isBorrowed ? classes.borrowed : classes.available
-      }`}
-    >
-      <span
-        className={`${classes.StockStateIcon} ${
-          isBorrowed ? classes.red : classes.green
+    <div className={classes.StockStateContainer}>
+      <div
+        className={`${classes.StockState} ${
+          isBorrowed ? classes.borrowed : classes.available
         }`}
-      />
-      {isBorrowed ? "貸出中" : "在庫あり"}
+      >
+        <span
+          className={`${classes.StockStateIcon} ${
+            isBorrowed ? classes.red : classes.green
+          }`}
+        />
+        {isBorrowed ? "貸出中" : "貸出可"}
+      </div>
     </div>
   );
 };
