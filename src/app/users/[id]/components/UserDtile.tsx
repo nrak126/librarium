f14550f8238de9eb8eed9c5a467dc29b6adcda9a
kkkk0113 <koto1658@gmail.com>
2025-03-29 22:25:22 +0900

    ユーザー詳細画面に「戻る」ボタンを追加し、ナビゲーション機能を強化。スタイルを調整。

--- src/app/users/[id]/components/UserDtile.tsx
+++ src/app/users/[id]/components/UserDtile.tsx
@@ -6,14 +6,23 @@ import Icon from "@/public/icon.svg";
 import { TagList } from "@/src/components/Users/TagList";
 import { TagEdit } from "@/src/components/Users/TagEdit";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { Btn } from "@/src/components/book/Btn";
 
 export default function UserDtile() {
   const [clickEditer, setClickEditer] = useState(false);
+  const router = useRouter();
 
   const handleSample = () => {
     setClickEditer(!clickEditer);
     console.log(`ClickEdit ${clickEditer}`);
     console.log("編集が押されました。");
+  };
+
+  const handleBack = () => {
+    console.log("Backが押されました。");
+    router.back();
+    return;
   };
 
   return (
@@ -52,6 +61,10 @@ export default function UserDtile() {
         <progress max="97" value="50">
           <p>50%</p>
         </progress>
+      </div>
+
+      <div className={styles.backbutton}>
+        <Btn text="戻る" bgColor="#99C6E2" onClick={handleBack} />
       </div>
     </div>
   );
