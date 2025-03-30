2ae787371139111467b1b2196feb2e357a33348d
nrak126 <daichidaichi126@gmail.com>
2025-03-30 17:52:12 +0900

    loansのリストを取得するAPIエンドポイントを追加

---
+++ src/app/api/loans/rentalList/route.ts
@@ -0,0 +1,24 @@
+// app/api/loans/route.ts
+import { NextResponse } from "next/server";
+import { supabase } from "@/src/lib/supabase";
+
+// GET: loansのリストを取得する
+export async function GET() {
+  try {
+    const { data, error } = await supabase
+      .from("loans")
+      .select(`
+        *,
+        books(*),
+        users(*)
+      `)
+
+    if (error) {
+      throw error;
+    }
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("Error fetching loans:", error);
+    return NextResponse.error();
+  }
+}
