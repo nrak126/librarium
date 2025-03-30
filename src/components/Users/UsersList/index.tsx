8e84a712cf7aeec8858d52579391f46b1fe62ad0
kkkk0113 <koto1658@gmail.com>
2025-03-30 14:07:47 +0900

    ユーザー一覧コンポーネントをリファクタリングし、ユーザーデータ表示をUserDataコンポーネントに移行

--- src/components/Users/UsersList/index.tsx
+++ src/components/Users/UsersList/index.tsx
@@ -1,44 +1,13 @@
 import styles from "./index.module.scss";
-import Image from "next/image";
 import { User } from "@/src/types";
+import UserData from "../UserData";
 
 function UsersList({ users }: { users: User[] }) {
   return (
     <>
-      <div className={styles.all}>
-        {users.map((data, index) => {
-          return (
-            <div key={index} className={styles.whole}>
-              <Image
-                src={data.icon}
-                alt={"librariumのアイコン"}
-                width={57}
-                height={57}
-                className={styles.icon}
-              />
-
-              <div className={styles.sub}>
-                <div className={styles.component}>
-                  <div className={styles.numname}>
-                    {data.studentId + " " + data.name}
-                  </div>
-                  <div className={styles.level}>Lv.{data.level}</div>
-                </div>
-
-                <div className={styles.tagComp}>
-                  {data.tags.slice(0, 4).map((tagdata, index) => {
-                    return (
-                      <div key={index} className={styles.tag}>
-                        #{tagdata}
-                      </div>
-                    );
-                  })}
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+      {users.map((data, index) => {
+        return <UserData user={data} key={index} />;
+      })}
     </>
   );
 }
