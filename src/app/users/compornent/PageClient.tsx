eec911cce8b009b2d3b8543ad7c76e3edcf7a4f0
kkkk0113 <koto1658@gmail.com>
2025-03-30 13:38:41 +0900

    ユーザー詳細表示コンポーネントの名前を修正し、ユーザー情報の取得方法を更新

--- src/app/users/compornent/PageClient.tsx
+++ src/app/users/compornent/PageClient.tsx
@@ -4,17 +4,8 @@ import { useState, useEffect } from "react";
 import { SearchBar } from "@/src/components/SearchBar";
 import styles from "./PageClient.module.scss";
 import UsersList from "@/src/components/Users/UsersList";
-import UserDate from "@/src/components/Users/UserData";
+import UserData from "@/src/components/Users/UserData";
 import { User } from "@/src/types";
-
-// type User = {
-//   usernum: number;
-//   icon: string;
-//   num: string;
-//   name: string;
-//   level: number;
-//   tag: string[];
-// };
 
 export function PageClient() {
   const [searchClick, setSearchClick] = useState(false);
@@ -32,7 +23,7 @@ export function PageClient() {
       setUsers(data);
     })();
   }, []);
-  
+
   useEffect(() => {
     if (searchWordClick === true) {
       const filteredUsers = result.filter((user) =>
@@ -61,7 +52,7 @@ export function PageClient() {
         </div>
       </div>
       <div className={styles.myprofile}>MY PROFILE</div>
-      <UserDate />
+      <UserData user={users[0]} />
       {searchClick ? (
         <div className={styles.titleSearch}>SEARCH</div>
       ) : (
--- src/components/Users/UserData/index.tsx
+++ src/components/Users/UserData/index.tsx
@@ -1,47 +1,45 @@
-import { userId } from "../userId";
 import styles from "./index.module.scss";
 import Image from "next/image";
-import Icon from "@/public/icon.svg";
+import { User } from "@/src/types";
 
-function UserDate() {
+function UserData({ user }: { user: User }) {
+  if (!user) {
+    return <h1>Loading...</h1>;
+  }
   return (
     <>
       <div className={styles.all}>
-        {userId.map((data, index) => {
-          return (
-            <div key={index} className={styles.whole}>
-              <Image
-                src={Icon}
-                alt={"librariumのアイコン"}
-                width={57}
-                height={57}
-                className={styles.icon}
-              />
+        <div className={styles.whole}>
+          <Image
+            src={user.icon}
+            alt={"ユーザーアイコン"}
+            width={57}
+            height={57}
+            className={styles.icon}
+          />
 
-              <div className={styles.sub}>
-                <div className={styles.component}>
-                  <div className={styles.numname}>
-                    {data.num + " " + data.name}
-                  </div>
-                  <div className={styles.level}>Lv.{data.level}</div>
-                </div>
-
-                <div className={styles.tagComp}>
-                  {data.tag.slice(0, 4).map((tagdata, index) => {
-                    return (
-                      <div key={index} className={styles.tag}>
-                        #{tagdata}
-                      </div>
-                    );
-                  })}
-                </div>
+          <div className={styles.sub}>
+            <div className={styles.component}>
+              <div className={styles.numname}>
+                {user.studentId + " " + user.name}
               </div>
+              <div className={styles.level}>Lv.{user.level}</div>
             </div>
-          );
-        })}
+
+            <div className={styles.tagComp}>
+              {user.tags.slice(0, 4).map((tagdata, index) => {
+                return (
+                  <div key={index} className={styles.tag}>
+                    #{tagdata}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
 }
 
-export default UserDate;
+export default UserData;
