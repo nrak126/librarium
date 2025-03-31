020a88b85c2cf5c7b2ad4ee2539d613c431c95f3
kkkk0113 <koto1658@gmail.com>
2025-03-31 11:30:30 +0900

    本検索機能の完成

--- src/app/books/[id]/page.tsx
+++ src/app/books/[id]/page.tsx
@@ -3,15 +3,23 @@ import { Book } from "@/src/types/book";
 import { BookInfo } from "@/src/components/book/BookInfo";
 import { BackBtn } from "./components/BackBtn";
 
-export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const { id } = await params;
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/books/${id}`, {
-    cache: "no-store",
-  });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/books/${id}`,
+    {
+      cache: "no-store",
+    }
+  );
 
   if (!res.ok) {
     // ここでエラーハンドリング（404ページ返すとか）
-    throw new Error(`HTTP error! status: ${res.status}`);
+    // throw new Error(`HTTP error! status: ${res.status}`);
+    return <h1>データの取得に失敗しました。</h1>;
   }
 
   const book: Book = await res.json();
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
 import { PageClient } from "./components/pageClient";
-import { SearchBar } from "../components/SearchBar";
-import { NavSlide } from "../components/nav/NavSlide";
 
-export default async function Home() {
-  return (
-    <>
-      <SearchBar />
-      <NavSlide />
-      <PageClient />
-    </>
-  );
+export default function Home() {
+  return <PageClient />;
 }
