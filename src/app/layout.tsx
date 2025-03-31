ba48e607ddba95a5ce7968a751f0b78f116193ed
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-03-31 09:58:04 +0900

    タイトルを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="ja">
+      <title>Librarium</title>
       <body>
         <LayoutClient>{children}</LayoutClient>
       </body>
