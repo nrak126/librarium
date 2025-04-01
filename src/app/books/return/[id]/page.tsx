6bf78f708ff8c21b7d1d69980356bf2e1e77b744
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-04-02 01:36:38 +0900

    本の表示関連の修正と削除されたスタイルファイルの削除

--- src/app/books/return/[id]/page.tsx
+++ src/app/books/return/[id]/page.tsx
@@ -1,11 +1,19 @@
+import LoadingBrown from "@/src/components/LoadingBrown";
 import { PageClient } from "./components/PageClient";
+import { Suspense } from "react";
 
 export default async function Page() {
   return (
     <>
-      {/* <BookInfo book={book} /> */}
-      <PageClient />
-      {/* <ReturnBtn book={book} /> */}
+      <Suspense
+        fallback={
+          <div>
+            <LoadingBrown />
+          </div>
+        }
+      >
+        <PageClient />
+      </Suspense>
     </>
   );
 }
