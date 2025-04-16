21c7ba64beedb5af426bde070dc49dc10045baea
nrak126 <daichidaichi126@gmail.com>
2025-04-16 10:17:22 +0900

    貸し出し、返却をしたときに貸出可能数の更新する機能の追加

--- src/app/api/books/return/route.ts
+++ src/app/api/books/return/route.ts
@@ -1,30 +1,44 @@
 import { supabase } from "@/src/lib/supabase";
 import { NextResponse } from "next/server";
 
-export function GET() {
-  return;
-}
-
-// POST: 貸し出し履歴を登録する
+// POST: 返却処理を行う
 export async function POST(request: Request) {
   try {
     const { isbn, uid } = await request.json();
     console.log("isbn:", isbn, uid);
 
-    const { data, error } = await supabase
+    const { data: book, error: bookFetchError } =  await supabase
+    .from("books")
+    .select("*")
+    .eq("isbn", isbn)
+    .single();
+      if (bookFetchError) {
+        throw bookFetchError;
+      }
+
+    const { error: bookError } = await supabase
+      .from("books")
+      .update({ available: book.available + 1 })
+      .eq("isbn", isbn);
+    if (bookError) {
+      throw bookError;
+    }
+
+    const { data, error: loanError } = await supabase
       .from("loans")
       .update({
         isReturned: true,
+        return_date: new Date().toISOString(),
       })
       .eq("isbn", isbn)
       .eq("uid", uid);
-    if (error) {
-      throw error;
+    if (loanError) {
+      throw loanError;
     }
-    console.log("Created loan:", data);
-    return NextResponse.json(data, { status: 201 });
+    console.log("Updated loan:", data);
+    return NextResponse.json(data, { status: 200 });
   } catch (error) {
-    console.error("Error creating loan:", error);
+    console.error("Error updating loan:", error);
     return NextResponse.error();
   }
 }
