7ab20e23fc742ad2339c76a6331c522f082d3dc4
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-04-18 15:24:08 +0900

    パス変更

--- src/app/books/components/PageClient.tsx
+++ src/app/books/components/PageClient.tsx
@@ -26,7 +26,7 @@ export default function PageClient() {
 
   useEffect(() => {
     (async () => {
-      const booksRes = await fetch(`$api/books`);
+      const booksRes = await fetch(`api/books`);
 
       const booksData: Book[] = await booksRes.json();
       setBooks(booksData);
