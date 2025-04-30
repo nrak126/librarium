c5128132722815d82f1c2e230a8c81f4fe4bb7a0
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-04-30 23:45:39 +0900

    PWAの設定を更新

--- src/types/next-pwa.d.ts
+++ src/types/next-pwa.d.ts
@@ -6,8 +6,6 @@ declare module "next-pwa" {
     register?: boolean;
     skipWaiting?: boolean;
     disable?: boolean;
-    // 必要に応じて拡張
-    [key: string]: any;
   };
 
   function withPWA(options: PWAOptions): (config: NextConfig) => NextConfig;
