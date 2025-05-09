281f910709acec0cb0e4e243d126fed10773cd2f
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-05-09 16:22:50 +0900

    ホームアイコンを修正

--- src/components/HamBtn/NavList.tsx
+++ src/components/HamBtn/NavList.tsx
@@ -3,10 +3,10 @@ import Icon2 from "@/src/components/HamBtn/Img/return_side.svg";
 import Icon3 from "@/src/components/HamBtn/Img/user_side.svg";
 import Icon4 from "@/src/components/HamBtn/Img/books_side.svg";
 import Icon5 from "@/src/components/HamBtn/Img/add_side.svg";
-import Icon6 from "@/src/components/HamBtn/Img/homeicon.svg";
+import Icon6 from "@/src/components/HamBtn/Img/home_icon.svg";
 
 export const navItems = [
-  { id: 1, link: "./", label: "ホーム", iconUrl: Icon6 },
+  { id: 1, link: "/", label: "ホーム", iconUrl: Icon6 },
   { id: 2, link: "/books/rental/barcode", label: "貸出", iconUrl: Icon1 },
   { id: 3, link: "/books/return", label: "返却", iconUrl: Icon2 },
   { id: 4, link: "/users", label: "利用者一覧", iconUrl: Icon3 },
