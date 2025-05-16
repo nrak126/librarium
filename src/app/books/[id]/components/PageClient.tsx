c884e2e619b72f71f7cb9f8b2ff5eb403e3c21c1
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-05-16 12:45:37 +0900

    bookIDページを早くした

---
+++ src/app/books/[id]/components/PageClient.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+// app/book/[id]/page.tsx
+import { Book } from "@/src/types/book";
+import { BookInfo } from "@/src/components/book/BookInfo";
+import { BackBtn } from "../components/BackBtn";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+
+type Props = {
+  isbn: string;
+};
+
+export const PageClient = () => {
+  const pathname = usePathname();
+  const isbn = pathname.split("/").pop() ?? "";
+
+  const [book, setBook] = useState<Book[] | null>();
+
+  // 本のデータ取得（キャッシュ優先）
+  useEffect(() => {
+    const cached = localStorage.getItem(`books-${isbn}`);
+    if (cached) {
+      setBook([JSON.parse(cached)]);
+      return;
+    }
+
+    (async () => {
+      const res = await fetch(`/api/books/${isbn}`);
+      const data = await res.json();
+      setBook([data]);
+      localStorage.setItem(`book-${isbn}`, JSON.stringify(data));
+    })();
+  }, [isbn]);
+
+  return (
+    <div>
+      {book && <BookInfo book={book[0]} />}
+      <div style={{ margin: "20px" }}>
+        <BackBtn />
+      </div>
+    </div>
+  );
+};
--- src/app/books/[id]/page.tsx
+++ src/app/books/[id]/page.tsx
@@ -1,33 +1,13 @@
 // app/book/[id]/page.tsx
-import { Book } from "@/src/types/book";
-import { BookInfo } from "@/src/components/book/BookInfo";
-import { BackBtn } from "./components/BackBtn";
+
+import { PageClient } from "./components/PageClient";
 
 export default async function Page({
   params,
 }: {
-  params: Promise<{ id: string }>;
+  params: Promise<{ isbn: string }>;
 }) {
-  const { id } = await params;
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/books/${id}`,
-    {
-      cache: "no-store",
-    }
-  );
+  const { isbn } = await params;
 
-  if (!res.ok) {
-    return <h1>データの取得に失敗しました。</h1>;
-  }
-
-  const book: Book = await res.json();
-
-  return (
-    <div>
-      <BookInfo book={book} />
-      <div style={{ margin: "20px" }}>
-        <BackBtn />
-      </div>
-    </div>
-  );
+  return <PageClient />;
 }
