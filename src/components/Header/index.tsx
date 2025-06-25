7a3946444acdd5241910b3c0b65cb2824159293e
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-06-25 11:41:28 +0900

    useRouterからLinkに変更

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,30 +1,23 @@
-"use client";
-
 import React from "react";
 import styles from "./index.module.scss";
 import { HamBtn } from "@/src/components/HamBtn";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 import Image from "next/image";
 import logo from "@/public/Librarium.svg"; // ロゴ画像のパスを適切
 
 export const Header = () => {
-  const router = useRouter();
-  const link = "/";
-
-  const onHome = (link: string) => {
-    router.push(link); // クリック時に指定されたリンクに遷移
-  };
   return (
     <div className={styles.headerContainer}>
       <div className={styles.header}>
-        <Image
-          src={logo}
-          alt="HomeIcon"
-          width={180}
-          height={80}
-          onClick={() => onHome(link)}
-          className={styles.h1}
-        />
+        <Link href="/">
+          <Image
+            src={logo}
+            alt="HomeIcon"
+            width={180}
+            height={80}
+            className={styles.h1}
+          />
+        </Link>
         <HamBtn />
       </div>
       <div className={styles.underSpace}></div>
