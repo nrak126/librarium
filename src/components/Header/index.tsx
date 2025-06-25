51f4a077a8c7f8c7f4c0a4d5e094de66932daa27
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-06-25 11:39:55 +0900

    Headerの文字をsvgに変更

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import styles from "./index.module.scss";
 import { HamBtn } from "@/src/components/HamBtn";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+import logo from "@/public/Librarium.svg"; // ロゴ画像のパスを適切
 
 export const Header = () => {
   const router = useRouter();
@@ -15,9 +17,14 @@ export const Header = () => {
   return (
     <div className={styles.headerContainer}>
       <div className={styles.header}>
-        <h1 onClick={() => onHome(link)} className={styles.h1}>
-          Librarium
-        </h1>
+        <Image
+          src={logo}
+          alt="HomeIcon"
+          width={180}
+          height={80}
+          onClick={() => onHome(link)}
+          className={styles.h1}
+        />
         <HamBtn />
       </div>
       <div className={styles.underSpace}></div>
