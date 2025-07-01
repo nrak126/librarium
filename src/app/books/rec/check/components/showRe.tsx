aa9b0b5c000672a751cb332d20b6354086e0111c
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2025-07-01 16:44:29 +0900

    AIから持ってきた、おすすめ分野、理由を表示

--- src/app/books/rec/check/components/showRe.tsx
+++ src/app/books/rec/check/components/showRe.tsx
@@ -2,14 +2,16 @@
 
 import { logedInUserAtom } from "@/src/atoms/atoms";
 import { useAtom } from "jotai";
-
-`use client`;
+import { useSearchParams } from "next/navigation";
 
 export const ShowRec = () => {
   const [loginUser] = useAtom(logedInUserAtom);
+  const searchParams = useSearchParams();
+  const reason = searchParams.get("reason");
   return (
     <>
       <p>あなたの興味のある分野は「{loginUser?.interest_tech}」です。</p>
+      <p>{reason}</p>
     </>
   );
 };
