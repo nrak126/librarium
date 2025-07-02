20242b09a89568caf5537f65edba4d20aa7a1af3
kkkk0113 <koto1658@gmail.com>
2025-07-02 17:58:14 +0900

    feat: ユーザーリストにログインユーザーを除外する機能を追加

--- src/components/Users/UsersList/index.tsx
+++ src/components/Users/UsersList/index.tsx
@@ -1,10 +1,17 @@
 import { User } from "@/src/types";
 import UserData from "../UserData";
+import { useAtom } from "jotai";
+import { logedInUserAtom } from "@/src/atoms/atoms";
 
 function UsersList({ users }: { users: User[] }) {
+  const [logedInUser] = useAtom<User | null>(logedInUserAtom);
+  console.log(logedInUser);
   return (
     <>
       {users.map((data) => {
+        if (logedInUser && data.id === logedInUser.id) {
+          return null;
+        }
         return <UserData user={data} key={data.id} />;
       })}
     </>
